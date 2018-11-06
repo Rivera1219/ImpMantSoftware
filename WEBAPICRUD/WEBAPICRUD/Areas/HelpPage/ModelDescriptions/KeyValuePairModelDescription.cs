0af3b5f81b5eea7cde96fc7f1072236477eea6da
Rivera1219 <riverab1219@gmail.com>
2018-11-05 23:23:54 -0600

    test
    test

--- WEBAPICRUD/WEBAPICRUD/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace WEBAPICRUD.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
--- WEBAPICRUD/WEBAPICRUD/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace WEBAPICRUD.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
